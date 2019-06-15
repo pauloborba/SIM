da68566e6c16b791bdd0003f57c102a190f5a322
arthur cabeca <acrc2@cin.ufpe.br>
2019-06-15 13:26:28 -0300

    [#30]Implementação do teste de serviço para quetões puxadas do the huxley

--- SIM-server/spec/sim-server.spec.ts
+++ SIM-server/spec/sim-server.spec.ts
@@ -1,6 +1,6 @@
 import request = require("request-promise");
 import 'jasmine';
-import { closeServer } from '../sim-server';
+//import { closeServer } from '../sim-server';
 
 var base_url = "http://localhost:3000/";
 
@@ -10,6 +10,13 @@ describe("O servidor", () => {
   beforeAll(() => {server = require('../sim-server')});
 
   afterAll(() => {server.closeServer()});
+
+
+  it("consegue receber as submissões do the huxley", () => {
+      return request.get(base_url + "submissoes").then(body => {
+        expect(body).toContain('[{"ID":0,"nomeSub":"Submissão 5","aluno":"Matheus","code":""},{"ID":1,"nomeSub":"Submissão 4","aluno":"Matheus","code":""},{"ID":2,"nomeSub":"Submissão 5","aluno":"Erica","code":""}]');
+      });
+  })
 
 
 })
