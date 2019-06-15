9ea123a02666efcbc5252e26f718eba89e3cffcd
aldibergjr <aldibergjr@gmail.com>
2019-06-15 01:35:26 -0300

    [#29]Criação de teste de serviço/classe Não cadastra monitor com nome duplicado

---
+++ SIM-server/spec/cadastromonitores.spec.ts
@@ -0,0 +1,41 @@
+import { CadastroMonitores } from '../cadastromonitores';
+import { Monitor } from '../../SIM-app/src/app/alocacao/monitor';
+
+describe("O cadastro de alunos", () => {
+  var cadastro: CadastroMonitores;
+
+  beforeEach(() => cadastro = new CadastroMonitores())
+
+  it("é inicialmente vazio", () => {
+    expect(cadastro.getMonitores().length).toBe(0);
+  })
+
+  it("não cadastra monitor duplicado", () => {
+    var monitor: Monitor = new Monitor();
+    monitor.nome = "Daniel";
+    monitor.disponibilidade = [false,true,false,true,false];
+    monitor.restricoes = [];
+    monitor.alocacoes = 0;
+    monitor.chefe = true;
+    cadastro.criar(monitor);
+
+    expect(cadastro.getMonitores().length).toBe(1);
+    monitor = cadastro.getMonitores()[0];
+    expect(monitor.nome).toBe("Daniel");
+    expect(monitor.disponibilidade).toBe([false,true,false,true,false]);
+    expect(monitor.restricoes).toBe([]);
+    expect(monitor.alocacoes).toBe(0);
+    expect(monitor.chefe).toBe(true);
+
+    var monitor2: Monitor = new Monitor();
+    monitor2.nome = "Daniel";
+    monitor2.disponibilidade = [false,true,false,true,false];
+    monitor2.restricoes = [];
+    monitor2.alocacoes = 0;
+    monitor2.chefe = true;
+    cadastro.criar(monitor2);
+
+    expect(cadastro.getMonitores().length).toBe(1);
+  })
+  
+})
