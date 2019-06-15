62ef08e4a5282f043fb4d1016ff632e696e5dce4
Daniel Bastos <dmrb2@cin.ufpe.br>
2019-06-15 19:32:08 -0300

    [#20] Implementacao cenario Mudanca de disponibilidade de monitor

--- SIM-app/src/app/alocacao/alteracao/alteracao.component.ts
+++ SIM-app/src/app/alocacao/alteracao/alteracao.component.ts
@@ -15,6 +15,9 @@ export class AlteracaoComponent implements OnInit {
   constructor(private aulaService: AulaService, private monitorService: MonitorService) { }
 
   monitores: Monitor[];
+  monitorSelect: Monitor;
+  nomeMonitor: string;
+
   aulas: Aula[] = [];
   data: string;
   diaSelect: Aula;
@@ -87,5 +90,73 @@ export class AlteracaoComponent implements OnInit {
       });
     }
     return result;
+  }
+
+  buscarMonitor() : void {
+    this.monitorSelect = this.monitores.find(m => m.nome == this.nomeMonitor);
+  }
+
+  atualizarMonitor() : void {
+    if(typeof this.monitorSelect.restricoes === "string")
+      this.monitorSelect.restricoes = this.getRestricoes();
+    this.monitorService.atualizar(this.monitorSelect)
+    .then(as => {
+      if(as) {
+        this.monitorSelect = undefined;
+        this.nomeMonitor = "";
+        for(var i = 0; i < this.monitores.length; i++) {
+          if (this.monitores[i].nome == as.nome) {
+            this.monitores[i] = as;
+          }
+        }
+        this.retirarAlocacoes(as);
+      }
+    })
+    .catch(erro => alert(erro));
+  }
+
+  getRestricoes() : string[]{
+    var result = [];
+    if(this.monitorSelect.restricoes == [""]) return result
+    else if (this.monitorSelect.restricoes.indexOf(",") < 0) {
+      result.push(this.monitorSelect.restricoes);
+    } else {
+      var rest = this.monitorSelect.restricoes.toString();
+      result = rest.split(",");
+    }
+    return result;
+  }
+
+  retirarAlocacoes(monitor: Monitor) : void {
+    this.aulaService.getAulas()
+    .then(as => this.aulas = as)
+    .catch(erro => alert(erro));
+    this.aulas.forEach(aula => {
+      if(!monitor.disponibilidade[this.getDia(aula.diaSemana)] && this.monitorAlocado(monitor, aula)) {
+        aula.monitores.splice(aula.monitores.findIndex(m => m.nome == monitor.nome));
+        this.aulaService.atualizar(aula)
+        .then(a => {
+          if(a) {
+            for(var i = 0; i < this.aulas.length; i++) {
+              if (this.aulas[i].data == a.data) {
+                this.aulas[i] = a;
+              }
+            }
+          }
+        })
+      }
+    })
+  }
+
+  getDia(dia: string) : number {
+    if(dia == "segunda-feira") return 0;
+    else if (dia == "terca-feira") return 1;
+    else if (dia == "quarta-feira") return 2;
+    else if (dia == "quinta-feira") return 3;
+    else return 4;
+  }
+
+  monitorAlocado(monitor: Monitor, aula: Aula) : boolean {
+    return aula.monitores.findIndex(m => m.nome == monitor.nome) > -1;
   }
 }
--- SIM-server/cadastromonitores.ts
+++ SIM-server/cadastromonitores.ts
@@ -1,7 +1,62 @@
 import { Monitor } from '../SIM-app/src/app/alocacao/monitor';
 
+var clean = function() : void {
+  this.nome = "";
+  this.disponibilidade = [false,false,false,false,false];
+  this.restricoes = [];
+  this.alocacoes = 0;
+  this.chefe = false;
+}
+
+var copyFrom = function(from: Monitor): void{
+  this.nome = from.nome;
+  this.copyDisponibilidade(from.disponibilidade);
+  this.copyRestricoes(from.restricoes);
+  this.alocacoes = from.alocacoes;
+  this.chefe = from.chefe
+}
+
+var copyDisponibilidade = function(from: boolean[]) : void {
+  this.disponibilidade = [false, false, false, false, false];
+  for(var i = 0; i < from.length; i++) {
+      this.disponibilidade[i] = from[i];
+  }
+}
+
+var copyRestricoes = function(from: string[]) : void {
+  this.restricoes = [];
+  for(var i = 0; i < from.length; i++) {
+      if(from[i] != "") this.restricoes[i] = from[i];
+  }
+}
 export class CadastroMonitores {
-  monitores: Monitor[] = [];
+  monitores: Monitor[] = [
+    {
+      nome: "Daniel",
+      disponibilidade: [true,false,true,false,false],
+      restricoes: [
+        "23/03",
+        "25/07"
+      ],
+      alocacoes: 1,
+      chefe: true,
+      clean: clean,
+      copyFrom: copyFrom,
+      copyDisponibilidade: copyDisponibilidade,
+      copyRestricoes: copyRestricoes
+    },
+    {
+      nome: "Ruy",
+      disponibilidade: [true,false,false,false,false],
+      restricoes: [],
+      alocacoes: 0,
+      chefe: true,
+      clean: clean,
+      copyFrom: copyFrom,
+      copyDisponibilidade: copyDisponibilidade,
+      copyRestricoes: copyRestricoes
+    }
+  ];
 
   criar(monitor: Monitor): Monitor {
     var result = null;
@@ -14,7 +69,9 @@ export class CadastroMonitores {
   }
 
   atualizar(monitor: Monitor) : Monitor {
-    return new Monitor();
+    var result : Monitor = this.monitores.find(m => m.nome == monitor.nome);
+    if (result) result.copyFrom(monitor);
+    return result;
   }
 
   getMonitores() : Monitor[] {
