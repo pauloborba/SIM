f4decdc17853c26a48da502461fdbff19314093c
Ruymelo10 <ruyovidio@gmail.com>
2019-06-16 01:30:20 -0300

    [FINISHES#25] Finalização da implementação do cenário Gerar alocações

--- tests-acceptance/features/alocacao.feature
+++ tests-acceptance/features/alocacao.feature
@@ -3,15 +3,16 @@ Feature: As professor
          So que eu possa possa aloca-los nas aulas       
 
 Scenario: Gerar Alocações
-Given Estou na página "SIMApp"
-Given Estou no menu "Alteracao"
-Given O monitor "Pedro" está cadastrado e disponível para "segunda-feira"
-Given O monitor "Jorge" está cadastrado e disponível para "quarta-feira"
-Given A aula "25/03" ainda não possui monitores
-Given A aula "27/03" ainda não possui monitores
-When Eu solicito a alocação de monitores
-Then O monitor "Pedro" aparece alocado na aula "25/03"
-Then O monitor "Jorge" aparece alocado na aula "27/03"
+Given Estou na página "SIMApp" 
+Given Estou no menu "Alteracao" 
+Given O monitor "Pedro" está cadastrado e disponível para "segunda-feira" 
+Given O monitor "Jorge" está cadastrado e disponível para "quarta-feira" 
+Given A aula "25/03" ainda não possui monitores 
+Given A aula "27/03" ainda não possui monitores 
+When Eu solicito a alocação do monitor "Pedro" na aula "25/03" 
+When Eu solicito a alocação do monitor "Jorge" na aula "27/03" 
+Then O monitor "Pedro" aparece alocado na aula "25/03" 
+Then O monitor "Jorge" aparece alocado na aula "27/03" 
 
 Scenario: Alteração manual de monitor para alocação em um dia especificado
 Given Estou na página "SIMApp"
--- tests-acceptance/stepdefinitions/alocacoes.ts
+++ tests-acceptance/stepdefinitions/alocacoes.ts
@@ -3,6 +3,11 @@ import { browser, $, element, ElementArrayFinder, by } from 'protractor';
 let chai = require('chai').use(require('chai-as-promised'));
 let expect = chai.expect;
 
+let sameName = ((elem, name) => elem.element(by.name('nome')).getText().then(text => text === name));
+let sameDay = ((elem, dia) => elem.element(by.name(dia)).getText().then(text => text === "true"));
+let pAND = ((p,q) => p.then(a => q.then(b => a && b)))
+let sameSome = ((elem, param, tagName) => elem.element(by.name(tagName)).getText().then(text => text === param));
+
 defineSupportCode(function ({ Given, When, Then }) {
     Given(/^Estou na página "([^\"]*)"$/, async (page) => {
         await browser.get("http://localhost:4200");
@@ -14,24 +19,13 @@ defineSupportCode(function ({ Given, When, Then }) {
     })
 
     Given(/^O monitor "([^\"]*)" está cadastrado e disponível para "([^\"]*)"$/, async (name,day) => {
-        await element(by.buttonText('Formulario')).click();
-        await $("input[name='nomeMonitor']").sendKeys(<string> name);
-        if(day == 'segunda-feira')
-        await $("input[name='segunda-feira']").click();
-        if(day == 'terca-feira')
-        await $("input[name='terca-feira']").click();
-        if(day == 'quarta-feira')
-        await $("input[name='quarta-feira']").click();
-        if(day == 'quinta-feira')
-        await $("input[name='quinta-feira']").click();
-        if(day == 'sexta-feira')
-        await $("input[name='sexts-feira']").click();
-        await element(by.buttonText('Disponibilidade')).click();
-        var monitores : ElementArrayFinder = element.all(by.repeater('let m of monitores'));
+        await $("a[name='alocacao']").click();
+        await $("button[name='disponibilidade']").click();
+
+        var monitores : ElementArrayFinder = element.all(by.name('monitoresList'));
         await monitores;
-        var monitorSelec = monitores.filter(element => element.column('m.nome') === name);
-        await monitorSelec;
-        await monitorSelec.then(elems => expect(Promise.resolve(elems.length)).to.eventually.equal(1));
+        var monitor = monitores.filter(elem => pAND(sameName(elem, name), sameDay(elem,day)));
+        await monitor.then(elems => expect(Promise.resolve(elems.length)).to.eventually.equal(1));
     })
     Given(/^A quantidade mínima de monitores da aula "([^\"]*)" é "([^\"]*)"$/, async (day, number) => {
         await element(by.buttonText('Cronograma')).click();
@@ -43,18 +37,24 @@ defineSupportCode(function ({ Given, When, Then }) {
     })
     
     Given(/^A aula "([^\"]*)" ainda não possui monitores$/, async (day) => {
-        await element(by.buttonText('Cronograma')).click();
-        var aulas : ElementArrayFinder = element.all(by.repeater('let a of aulas'));
+        await $("a[name='alocacao']").click();
+        await $("button[name='cronograma']").click();
+        var aulas : ElementArrayFinder = element.all(by.name('cronogramaTabela'));
         await aulas;
-        var aula = aulas.filter(element => element.column('a.data') === day);
-        await aula;
+        var aula = aulas.filter(elem => sameSome(elem, day, 'data'));
         await aula.then(elems => expect(Promise.resolve(elems.length)).to.eventually.equal(1));
-        var monitorEncontrado = aula.all(by.repeater('let m of a.monitores'));
-        await monitorEncontrado.then(elems => expect(Promise.resolve(elems.length)).to.eventually.equal(0));
+        var findmonitor = aula.all(by.name('monitoresAlocados'));
+        await findmonitor;
+        var alocado = findmonitor.filter(element => (element.getText()).then(e => e === 'Qualquer monitor'));
+        await alocado.then(elems => expect(Promise.resolve(elems.length)).to.eventually.equal(0));
     });
 
-    When(/^Eu solicito a alocação de monitores$/, async () => {
-        await element(by.buttonText('Cronograma')).click();
+    When(/^Eu solicito a alocação do monitor "([^\"]*)" na aula "([^\"]*)"$/, async (name,day) => {
+        await $("a[name='alocacao']").click();
+        await $("button[name='alteracao']").click();
+        await $("input[name='buscadia']").sendKeys(<string>day);
+        await $("button[name='buscar']").click();
+        await $("input[name='monitoresAlocados']").sendKeys(<string>name);
         await element(by.buttonText('Alocar Monitores')).click();
     })
     When(/^Eu edito os monitores de "([^\"]*)", adicionando o monitor "([^\"]*)"$/, async (day,name) => {
@@ -83,16 +83,16 @@ defineSupportCode(function ({ Given, When, Then }) {
         await aula.then(elems => expect(Promise.resolve(elems.length)).to.eventually.equal(1));
     })
     Then(/^O monitor "([^\"]*)" aparece alocado na aula "([^\"]*)"$/, async (name, day) => {
-        await element(by.buttonText('Cronograma')).click();
-        var aulas : ElementArrayFinder = element.all(by.repeater('let a of aulas'));
+        await $("a[name='alocacao']").click();
+        await $("button[name='cronograma']").click();
+        var aulas : ElementArrayFinder = element.all(by.name('cronogramaTabela'));
         await aulas;
-        var aula = aulas.filter(element => element.column('a.data') === day);
-        await aula;
+        var aula = aulas.filter(elem => sameSome(elem, day, 'data'));
         await aula.then(elems => expect(Promise.resolve(elems.length)).to.eventually.equal(1));
-        var monitorEncontrado = aula.all(by.repeater('let m of a.monitores'));
-        await monitorEncontrado;
-        var monitorAlocado = monitorEncontrado.filter(element => expect(element.getText()).then(e => e === name));
-        await monitorAlocado.then(elems => expect(Promise.resolve(elems.length)).to.eventually.equal(1));
+        var findmonitor = aula.all(by.name('monitoresAlocados'));
+        await findmonitor;
+        var alocado = findmonitor.filter(element => (element.getText()).then(e => e === name));
+        await alocado.then(elems => expect(Promise.resolve(elems.length)).to.eventually.equal(1));
     })
     Then(/^No menu "([^\"]*)" aparece uma mensagem de erro ao lado da aula "([^\"]*)"$/, async (name, day) => {
         await element(by.buttonText(<string> name)).click();
